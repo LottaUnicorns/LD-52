4b048238f11e9b0d8163cc0c4d04d4c54e8d7079
lotta <lotta@sparklingsociety.net>
2023-01-09 15:59:50 +0100

    changed sound of footsteps a little

--- New Unity Project/Assets/Scripts/ShowTutorial.cs
+++ New Unity Project/Assets/Scripts/ShowTutorial.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ShowTutorial : MonoBehaviour
 {
+    [SerializeField] GameObject tutorialPanel;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        tutorialPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (tutorialPanel == false)
+            {
+                tutorialPanel.SetActive(true);
+            }
+            else if (tutorialPanel == true)
+            {
+                tutorialPanel.SetActive(true);
+            }
+            
+        }
     }
 }
